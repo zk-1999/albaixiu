e0f8491276df0503682985a251d1faf9f8d9f6cf
zk-1999 <120362753@qq.com>
2019-07-04 15:27:49 +0800

    批量删除用户

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -82,13 +82,58 @@ $('#modifyBox').on('submit', '#userForm', function() {
         })
         return false;
     })
-    //用户删除功能
+    //用户根据id删除用户功能
 $('#userBox').on('click', '.delete', function() {
-    if (confirm('您真的要删除用户吗?')) {
-        var id = $(this).attr('data-id');
+        if (confirm('您真的要删除用户吗?')) {
+            var id = $(this).attr('data-id');
+            $.ajax({
+                type: 'delete',
+                url: '/users/' + id,
+                success: function(result) {
+                    location.reload();
+                },
+                error: function(error) {
+                    console.log(error)
+                }
+            })
+        }
+    })
+    //当切换全选input的时候，下面所有的input跟着来改变
+$('#selectAll').on('change', function() {
+        var bool = $(this).prop('checked');
+        $('#userBox').find('.status').prop('checked', bool);
+        if (bool == true) {
+            $('#deleteMany').show();
+        } else {
+            $('#deleteMany').hide();
+        }
+    })
+    //当tbody中的input全部选中的时候，我们就让全选也是选中状态
+$('#userBox').on('change', '.status', function() {
+        if ($('#userBox').find('.status').length == $('#userBox').find('.status').filter(':checked').length) {
+            $('#selectAll').prop('checked', true);
+        } else {
+            $('#selectAll').prop('checked', false);
+        }
+        if ($('#userBox').find('.status').filter(':checked').length >= 2) {
+            $('#deleteMany').show();
+        } else {
+            $('#deleteMany').hide();
+        }
+    })
+    //批量删除用户
+$('#deleteMany').on('click', function() {
+    if (confirm('您真的要删除用户吗')) {
+        var selectAll = $('#userBox').find('.status').filter(':checked');
+        var arr = [];
+        // console.log(selectAll);
+        selectAll.each(function(index, element) {
+            // console.log($(element).attr('data-id'));
+            arr.push($(element).attr('data-id'));
+        })
         $.ajax({
             type: 'delete',
-            url: '/users/' + id,
+            url: '/users/' + arr.join('-'),
             success: function(result) {
                 location.reload();
             },
@@ -97,5 +142,4 @@ $('#userBox').on('click', '.delete', function() {
             }
         })
     }
-
 })
